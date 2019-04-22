8c44b87551fab11c08635001c15552ad9a1942bd
yunjin choi <zzdd1558@naver.com>
2019-04-22 14:38:50 +0900

    Django , React 연동 테스트

---
+++ backend/post/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Post
+
+class PostSerializer(serializers.ModelSerializer) :
+
+    class Meta :
+        fields = (
+            'id',
+            'title',
+            'content'
+        )
+
+        model = Post
